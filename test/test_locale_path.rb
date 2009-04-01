9a67306834390526e9dbd79814b475283826c5fc
OZAWA Sakuro <github@2238club.org>
2009-04-01 17:38:38 +0800

    Fix test_initialize_with_topdir to match with new LocalePath#locale_paths implementation.
    Signed-off-by: Masao Mutoh <mutoh@highway.ne.jp>

--- test/test_locale_path.rb
+++ test/test_locale_path.rb
@@ -28,8 +28,8 @@ class TestLocalePath < Test::Unit::TestCase
   def test_initialize_with_topdir
     testdir = File.dirname(File.expand_path(__FILE__))
     path = GetText::LocalePath.new("test1", "#{testdir}/locale")
-    assert_equal path.locale_paths, ["#{testdir}/locale/%{lang}/LC_MESSAGES/test1.mo", 
-                                     "#{testdir}/locale/%{lang}/test1.mo"]
+    assert_equal path.locale_paths, { "ja" => "#{testdir}/locale/ja/LC_MESSAGES/test1.mo", 
+                                     "fr" => "#{testdir}/locale/fr/LC_MESSAGES/test1.mo"}
     assert_equal path.current_path(Locale::Tag.parse("ja")), "#{testdir}/locale/ja/LC_MESSAGES/test1.mo"
     assert_equal path.current_path(Locale::Tag.parse("ja-JP")), "#{testdir}/locale/ja/LC_MESSAGES/test1.mo"
     assert_equal path.current_path(Locale::Tag.parse("ja_JP.UTF-8")), "#{testdir}/locale/ja/LC_MESSAGES/test1.mo"
