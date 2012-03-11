9b45f7c1580c2db2851c6b0d5c51af763150f8ba
Kouhei Sutou <kou@clear-code.com>
2012-03-11 23:18:27 +0900

    Fix copyright order

--- lib/gettext/version.rb
+++ lib/gettext/version.rb
@@ -1,8 +1,8 @@
 =begin
   version - version information of Ruby-GetText-Package
 
-  Copyright (C) 2005-2009 Masao Mutoh
   Copyright (C) 2012 Kouhei Sutou <kou@clear-code.com>
+  Copyright (C) 2005-2009 Masao Mutoh
 
   You may redistribute it and/or modify it under the same
   license terms as Ruby or LGPL.
