f7b4abf2af1fbc6730268cfe8ca8600515f291e0
Haruka Yoshihara <yoshihara@clear-code.com>
2012-08-21 12:38:02 +0900

    xgettext: add command line options for package name, version, copyright holder, msgid bugs address
    refs #7, #8
    
    http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=684187
    
    Reported by Francesco Poli (wintermute) in Debian bug tracking system,
    and reported in github and given patch by 375gu. Thanks!!!

--- test/tools/test_xgettext.rb
+++ test/tools/test_xgettext.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012  Haruka Yoshihara <yoshihara@clear-code.com>
 #
 # License: Ruby's or LGPL
 #
@@ -56,23 +57,94 @@ msgstr ""
 EOP
   end
 
+  class TestCommandLineOption < self
+    def test_package_name
+      File.open(@rb_file_path, "w") do |rb_file|
+        rb_file.puts(header)
+      end
+
+      package_name = "test-package"
+      @xgettext.run("--output", @pot_file_path,
+                    "--package-name", package_name,
+                    @rb_file_path)
+
+      options = {:package_name => package_name}
+      expected_header = "#{header(options)}\n"
+      assert_equal(expected_header, File.read(@pot_file_path))
+    end
+
+    def test_package_version
+      File.open(@rb_file_path, "w") do |rb_file|
+        rb_file.puts(header)
+      end
+
+      package_version = "1.2.3"
+      @xgettext.run("--output", @pot_file_path,
+                    "--package-version", package_version,
+                    @rb_file_path)
+
+      options = {:package_version => package_version}
+      expected_header = "#{header(options)}\n"
+      assert_equal(expected_header, File.read(@pot_file_path))
+    end
+
+    def test_report_msgid_bugs_to
+      File.open(@rb_file_path, "w") do |rb_file|
+        rb_file.puts(header)
+      end
+
+      msgid_bugs_address = "me@example.com"
+      @xgettext.run("--output", @pot_file_path,
+                    "--msgid-bugs-address", msgid_bugs_address,
+                    @rb_file_path)
+
+      options = {:msgid_bugs_address => msgid_bugs_address}
+      expected_header = "#{header(options)}\n"
+      assert_equal(expected_header, File.read(@pot_file_path))
+    end
+
+    def test_copyright_holder
+      File.open(@rb_file_path, "w") do |rb_file|
+        rb_file.puts(header)
+      end
+
+      copyright_holder = "me"
+      @xgettext.run("--output", @pot_file_path,
+                    "--copyright-holder", copyright_holder,
+                    @rb_file_path)
+
+      options = {:copyright_holder => copyright_holder}
+      expected_header = "#{header(options)}\n"
+      assert_equal(expected_header, File.read(@pot_file_path))
+    end
+  end
+
   private
-  def header
+  def header(options=nil)
+    options ||= {}
+    package_name = options[:package_name] || "PACKAGE"
+    package_version = options[:package_version] || "VERSION"
+    msgid_bugs_address = options[:msgid_bugs_address] || ""
+    copyright_holder = options[:copyright_holder] ||
+                         "THE PACKAGE'S COPYRIGHT HOLDER"
+
     time = @now.strftime("%Y-%m-%d %H:%M%z")
     <<-"EOH"
 # SOME DESCRIPTIVE TITLE.
-# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
-# This file is distributed under the same license as the PACKAGE package.
+# Copyright (C) YEAR #{copyright_holder}
+# This file is distributed under the same license as the #{package_name} package.
 # FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
 #
 #, fuzzy
 msgid ""
 msgstr ""
-"Project-Id-Version: PACKAGE VERSION\\n"
+"Project-Id-Version: #{package_name} #{package_version}\\n"
+"Report-Msgid-Bugs-To: #{msgid_bugs_address}\\n"
 "POT-Creation-Date: #{time}\\n"
 "PO-Revision-Date: #{time}\\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\\n"
 "Language-Team: LANGUAGE <LL@li.org>\\n"
+"Language: \\n"
 "MIME-Version: 1.0\\n"
 "Content-Type: text/plain; charset=UTF-8\\n"
 "Content-Transfer-Encoding: 8bit\\n"
