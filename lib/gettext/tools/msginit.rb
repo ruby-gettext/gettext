ed69fd884a154319e391fc5c62764af848d21dc0
Haruka Yoshihara <yoshihara@clear-code.com>
2012-08-27 13:17:53 +0900

    msginit: add missing require

--- lib/gettext/tools/msginit.rb
+++ lib/gettext/tools/msginit.rb
@@ -19,6 +19,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require "gettext"
+require "gettext/tools/poparser"
+require "gettext/tools/msgmerge"
 require "locale/info"
 require "optparse"
 
