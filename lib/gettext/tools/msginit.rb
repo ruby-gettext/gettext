20502da07f49187dfec96ad8c27746042d49cc02
Haruka Yoshihara <yoshihara@clear-code.com>
2012-10-12 18:12:43 +0900

    msginit: support plural_forms for Chinese

--- lib/gettext/tools/msginit.rb
+++ lib/gettext/tools/msginit.rb
@@ -281,7 +281,7 @@ module GetText
 
       def plural_forms(language) #:nodoc:
         case language
-        when "ja", "vi", "ko"
+        when "ja", "vi", "ko", /zh.*/
           nplural = "1"
           plural_expression = "0"
         when "en", "de", "nl", "sv", "da", "no", "fo", "es", "pt",
