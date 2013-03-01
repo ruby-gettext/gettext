58b539f10c2e29abb64cd26a8fd7f2154e1daa96
Kouhei Sutou <kou@clear-code.com>
2013-03-01 22:51:52 +0900

    test: remove needless iconv require
    Because Ruby 2.0.0 drops iconv.
    
    GitHub: fix #14
    
    Reported by mtasaka. Thanks!!!

--- test/tools/test_xgettext.rb
+++ test/tools/test_xgettext.rb
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require "iconv"
 require "locale"
 require "gettext/tools/xgettext"
 
