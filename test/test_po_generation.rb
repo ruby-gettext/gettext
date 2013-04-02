807d9c8068c0ff29ff40f64f296931fe20b907a0
Kouhei Sutou <kou@clear-code.com>
2013-04-02 13:54:50 +0900

    Suppress warnings
        gettext/test/test_po_generation.rb:42: warning: ambiguous first argument; put parentheses or even spaces
        gettext/test/test_po_generation.rb:43: warning: ambiguous first argument; put parentheses or even spaces

--- test/test_po_generation.rb
+++ test/test_po_generation.rb
@@ -39,7 +39,7 @@ class TestPoGeneration < Test::Unit::TestCase
     # created file: File.open('/tmp/test.po', 'w').write(res)
 
     assert_match '#. "Fran\u00e7ois" or (with HTML entities) "Fran&ccedil;ois".', res
-    assert_no_match /Ignored/, res, 'Only comments starting with TRANSLATORS should be extracted'
-    assert_no_match /TRANSLATORS: This is a proper name/, res, 'The prefix "TRANSLATORS:" should be skipped'
+    assert_no_match(/Ignored/, res, 'Only comments starting with TRANSLATORS should be extracted')
+    assert_no_match(/TRANSLATORS: This is a proper name/, res, 'The prefix "TRANSLATORS:" should be skipped')
   end
 end
