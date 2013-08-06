985fa120fb33594182c7502ac7edeebe414a1a7c
Kouhei Sutou <kou@clear-code.com>
2013-08-06 22:49:29 +0900

    Remove deprecated lib/gettext/parser/
    It is backward incompatible change.

--- lib/gettext/parser/erb.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-warn "DEPRECATED: Use 'gettext/tools/parser/erb' instead."
-
-require 'gettext/tools/parser/erb'
--- lib/gettext/parser/glade.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-warn "DEPRECATED: Use 'gettext/tools/parser/glade' instead."
-
-require 'gettext/tools/parser/glade'
