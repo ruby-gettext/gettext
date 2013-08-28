eaab10e02b77b33bff708e0abb5bc6c4c433dc88
Kouhei Sutou <kou@clear-code.com>
2013-08-28 21:29:52 +0900

    Indent

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -34,104 +34,104 @@ class TestGetTextParser < Test::Unit::TestCase
   end
 
   class TestRuby < self
-  def test_ruby
-    @ary = @xgettext.parse(['fixtures/_.rb'])
+    def test_ruby
+      @ary = @xgettext.parse(['fixtures/_.rb'])
 
-    assert_target 'aaa\n', ['fixtures/_.rb:34']
-    assert_target 'bbb\nccc', ['fixtures/_.rb:38']
-    assert_target 'bbb\nccc\nddd\n', ['fixtures/_.rb:42']
-    assert_target 'eee', ['fixtures/_.rb:49', 'fixtures/_.rb:53']
-    assert_target 'fff', ['fixtures/_.rb:53']
-    assert_target 'ggghhhiii', ['fixtures/_.rb:57']
-    assert_target 'a"b"c"', ['fixtures/_.rb:63']
-    assert_target 'd"e"f"', ['fixtures/_.rb:67']
-    assert_target 'jjj', ['fixtures/_.rb:71']
-    assert_target 'kkk', ['fixtures/_.rb:72']
-    assert_target 'lllmmm', ['fixtures/_.rb:76']
-    assert_target 'nnn\nooo', ['fixtures/_.rb:84']
-    assert_target "\#", ['fixtures/_.rb:88', 'fixtures/_.rb:92']
-    assert_target "\\taaa", ['fixtures/_.rb:96']
-    assert_target "Here document1\\nHere document2\\n", ['fixtures/_.rb:100']
-    assert_target "Francois Pinard", ['fixtures/_.rb:120'] do |t|
-      assert_match(/proper name/, t.extracted_comment)
-      assert_match(/Pronunciation/, t.extracted_comment)
+      assert_target 'aaa\n', ['fixtures/_.rb:34']
+      assert_target 'bbb\nccc', ['fixtures/_.rb:38']
+      assert_target 'bbb\nccc\nddd\n', ['fixtures/_.rb:42']
+      assert_target 'eee', ['fixtures/_.rb:49', 'fixtures/_.rb:53']
+      assert_target 'fff', ['fixtures/_.rb:53']
+      assert_target 'ggghhhiii', ['fixtures/_.rb:57']
+      assert_target 'a"b"c"', ['fixtures/_.rb:63']
+      assert_target 'd"e"f"', ['fixtures/_.rb:67']
+      assert_target 'jjj', ['fixtures/_.rb:71']
+      assert_target 'kkk', ['fixtures/_.rb:72']
+      assert_target 'lllmmm', ['fixtures/_.rb:76']
+      assert_target 'nnn\nooo', ['fixtures/_.rb:84']
+      assert_target "\#", ['fixtures/_.rb:88', 'fixtures/_.rb:92']
+      assert_target "\\taaa", ['fixtures/_.rb:96']
+      assert_target "Here document1\\nHere document2\\n", ['fixtures/_.rb:100']
+      assert_target "Francois Pinard", ['fixtures/_.rb:120'] do |t|
+        assert_match(/proper name/, t.extracted_comment)
+        assert_match(/Pronunciation/, t.extracted_comment)
+      end
+
+      assert_target("No TRANSLATORS comment", ["fixtures/_.rb:123"]) do |t|
+        assert_nil(t.comment)
+      end
+
+      assert_target "self explaining", ['fixtures/_.rb:128'] do |t|
+        assert_nil t.comment
+      end
+
+      assert_target "This is a # including string.", ["fixtures/_.rb:132"]
+
+      # TODO: assert_target "in_quote", ['fixtures/_.rb:118']
     end
 
-    assert_target("No TRANSLATORS comment", ["fixtures/_.rb:123"]) do |t|
-      assert_nil(t.comment)
+    def test_ruby_N
+      @ary = @xgettext.parse(['fixtures/N_.rb'])
+
+      assert_target 'aaa', ['fixtures/N_.rb:10']
+      assert_target 'aaa\n', ['fixtures/N_.rb:14']
+      assert_target 'bbb\nccc', ['fixtures/N_.rb:18']
+      assert_target 'bbb\nccc\nddd\n', ['fixtures/N_.rb:22']
+      assert_target 'eee', ['fixtures/N_.rb:29', 'fixtures/N_.rb:33']
+      assert_target 'fff', ['fixtures/N_.rb:33']
+      assert_target 'ggghhhiii', ['fixtures/N_.rb:37']
+      assert_target 'a"b"c"', ['fixtures/N_.rb:43']
+      assert_target 'd"e"f"', ['fixtures/N_.rb:47']
+      assert_target 'jjj', ['fixtures/N_.rb:51']
+      assert_target 'kkk', ['fixtures/N_.rb:52']
+      assert_target 'lllmmm', ['fixtures/N_.rb:56']
+      assert_target 'nnn\nooo', ['fixtures/N_.rb:64']
     end
 
-    assert_target "self explaining", ['fixtures/_.rb:128'] do |t|
-      assert_nil t.comment
+    def test_ruby_n
+      @ary = @xgettext.parse(['fixtures/n_.rb'])
+      assert_plural_target "aaa", "aaa2", ['fixtures/n_.rb:29']
+      assert_plural_target "bbb\\n", "ccc2\\nccc2", ['fixtures/n_.rb:33']
+      assert_plural_target "ddd\\nddd", "ddd2\\nddd2", ['fixtures/n_.rb:37']
+      assert_plural_target "eee\\neee\\n", "eee2\\neee2\\n", ['fixtures/n_.rb:42']
+      assert_plural_target "ddd\\neee\\n", "ddd\\neee2", ['fixtures/n_.rb:48']
+      assert_plural_target "fff", "fff2", ['fixtures/n_.rb:55', 'fixtures/n_.rb:59']
+      assert_plural_target "ggg", "ggg2", ['fixtures/n_.rb:59']
+      assert_plural_target "ggghhhiii", "jjjkkklll", ['fixtures/n_.rb:63']
+      assert_plural_target "a\"b\"c\"", "a\"b\"c\"2", ['fixtures/n_.rb:72']
+      assert_plural_target "mmmmmm", "mmm2mmm2", ['fixtures/n_.rb:80']
+      assert_plural_target "nnn", "nnn2", ['fixtures/n_.rb:81']
+      assert_plural_target "comment", "comments", ['fixtures/n_.rb:97'] do |t|
+        assert_equal "please provide translations for all\n the plural forms!",
+                       t.extracted_comment
+      end
     end
 
-    assert_target "This is a # including string.", ["fixtures/_.rb:132"]
-
-    # TODO: assert_target "in_quote", ['fixtures/_.rb:118']
-  end
-
-  def test_ruby_N
-    @ary = @xgettext.parse(['fixtures/N_.rb'])
-
-    assert_target 'aaa', ['fixtures/N_.rb:10']
-    assert_target 'aaa\n', ['fixtures/N_.rb:14']
-    assert_target 'bbb\nccc', ['fixtures/N_.rb:18']
-    assert_target 'bbb\nccc\nddd\n', ['fixtures/N_.rb:22']
-    assert_target 'eee', ['fixtures/N_.rb:29', 'fixtures/N_.rb:33']
-    assert_target 'fff', ['fixtures/N_.rb:33']
-    assert_target 'ggghhhiii', ['fixtures/N_.rb:37']
-    assert_target 'a"b"c"', ['fixtures/N_.rb:43']
-    assert_target 'd"e"f"', ['fixtures/N_.rb:47']
-    assert_target 'jjj', ['fixtures/N_.rb:51']
-    assert_target 'kkk', ['fixtures/N_.rb:52']
-    assert_target 'lllmmm', ['fixtures/N_.rb:56']
-    assert_target 'nnn\nooo', ['fixtures/N_.rb:64']
-  end
-
-  def test_ruby_n
-    @ary = @xgettext.parse(['fixtures/n_.rb'])
-    assert_plural_target "aaa", "aaa2", ['fixtures/n_.rb:29']
-    assert_plural_target "bbb\\n", "ccc2\\nccc2", ['fixtures/n_.rb:33']
-    assert_plural_target "ddd\\nddd", "ddd2\\nddd2", ['fixtures/n_.rb:37']
-    assert_plural_target "eee\\neee\\n", "eee2\\neee2\\n", ['fixtures/n_.rb:42']
-    assert_plural_target "ddd\\neee\\n", "ddd\\neee2", ['fixtures/n_.rb:48']
-    assert_plural_target "fff", "fff2", ['fixtures/n_.rb:55', 'fixtures/n_.rb:59']
-    assert_plural_target "ggg", "ggg2", ['fixtures/n_.rb:59']
-    assert_plural_target "ggghhhiii", "jjjkkklll", ['fixtures/n_.rb:63']
-    assert_plural_target "a\"b\"c\"", "a\"b\"c\"2", ['fixtures/n_.rb:72']
-    assert_plural_target "mmmmmm", "mmm2mmm2", ['fixtures/n_.rb:80']
-    assert_plural_target "nnn", "nnn2", ['fixtures/n_.rb:81']
-    assert_plural_target "comment", "comments", ['fixtures/n_.rb:97'] do |t|
-      assert_equal "please provide translations for all\n the plural forms!",
-                     t.extracted_comment
+    def test_ruby_p
+      @ary = @xgettext.parse(['fixtures/p_.rb'])
+      assert_target_in_context "AAA", "BBB", ["fixtures/p_.rb:29", "fixtures/p_.rb:33"]
+      assert_target_in_context "AAA|BBB", "CCC", ["fixtures/p_.rb:37"]
+      assert_target_in_context "AAA", "CCC", ["fixtures/p_.rb:41"]
+      assert_target_in_context "CCC", "BBB", ["fixtures/p_.rb:45"]
+      assert_target_in_context "program", "name", ['fixtures/p_.rb:55'] do |t|
+        assert_equal "please translate 'name' in the context of 'program'.\n Hint: the translation should NOT contain the translation of 'program'.", t.extracted_comment
+      end
     end
-  end
-
-  def test_ruby_p
-    @ary = @xgettext.parse(['fixtures/p_.rb'])
-    assert_target_in_context "AAA", "BBB", ["fixtures/p_.rb:29", "fixtures/p_.rb:33"]
-    assert_target_in_context "AAA|BBB", "CCC", ["fixtures/p_.rb:37"]
-    assert_target_in_context "AAA", "CCC", ["fixtures/p_.rb:41"]
-    assert_target_in_context "CCC", "BBB", ["fixtures/p_.rb:45"]
-    assert_target_in_context "program", "name", ['fixtures/p_.rb:55'] do |t|
-      assert_equal "please translate 'name' in the context of 'program'.\n Hint: the translation should NOT contain the translation of 'program'.", t.extracted_comment
-    end
-  end
   end
 
   class TestGlade < self
-  def test_glade
-    # Old style (~2.0.4)
-    ary = GetText::GladeParser.parse('fixtures/gladeparser.glade')
+    def test_glade
+      # Old style (~2.0.4)
+      ary = GetText::GladeParser.parse('fixtures/gladeparser.glade')
 
-    assert_equal(['window1', 'fixtures/gladeparser.glade:8'], ary[0])
-    assert_equal(['normal text', 'fixtures/gladeparser.glade:29'], ary[1])
-    assert_equal(['1st line\n2nd line\n3rd line', 'fixtures/gladeparser.glade:50'], ary[2])
-    assert_equal(['<span color="red" weight="bold" size="large">markup </span>', 'fixtures/gladeparser.glade:73'], ary[3])
-    assert_equal(['<span color="red">1st line markup </span>\n<span color="blue">2nd line markup</span>', 'fixtures/gladeparser.glade:94'], ary[4])
-    assert_equal(['<span>&quot;markup&quot; with &lt;escaped strings&gt;</span>', 'fixtures/gladeparser.glade:116'], ary[5])
-    assert_equal(['duplicated', 'fixtures/gladeparser.glade:137', 'fixtures/gladeparser.glade:158'], ary[6])
-  end
+      assert_equal(['window1', 'fixtures/gladeparser.glade:8'], ary[0])
+      assert_equal(['normal text', 'fixtures/gladeparser.glade:29'], ary[1])
+      assert_equal(['1st line\n2nd line\n3rd line', 'fixtures/gladeparser.glade:50'], ary[2])
+      assert_equal(['<span color="red" weight="bold" size="large">markup </span>', 'fixtures/gladeparser.glade:73'], ary[3])
+      assert_equal(['<span color="red">1st line markup </span>\n<span color="blue">2nd line markup</span>', 'fixtures/gladeparser.glade:94'], ary[4])
+      assert_equal(['<span>&quot;markup&quot; with &lt;escaped strings&gt;</span>', 'fixtures/gladeparser.glade:116'], ary[5])
+      assert_equal(['duplicated', 'fixtures/gladeparser.glade:137', 'fixtures/gladeparser.glade:158'], ary[6])
+    end
   end
 
   class TestErbParser < self
