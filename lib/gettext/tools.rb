14e455b5f9a58f7749c87e0890b060e81ca2ff2f
Kouhei Sutou <kou@clear-code.com>
2014-02-02 22:53:30 +0900

    Use " (double quote) for string literal

--- lib/gettext/tools.rb
+++ lib/gettext/tools.rb
@@ -18,9 +18,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'gettext/tools/xgettext'
-require 'gettext/tools/msgfmt'
-require 'gettext/tools/msginit'
-require 'gettext/tools/msgmerge'
-require 'gettext/tools/msgcat'
-require 'gettext/mo'
+require "gettext/tools/xgettext"
+require "gettext/tools/msgfmt"
+require "gettext/tools/msginit"
+require "gettext/tools/msgmerge"
+require "gettext/tools/msgcat"
+require "gettext/mo"
