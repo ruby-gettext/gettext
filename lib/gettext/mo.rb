3e79bcfda815d9a2711860554f15d6d628665026
Kouhei Sutou <kou@clear-code.com>
2014-09-01 23:11:53 +0900

    Fix wrong msgid encoding
    It should be encoding specified by Content-Type.
    
    Patch by OBATA Akio. Thanks!!!

--- lib/gettext/mo.rb
+++ lib/gettext/mo.rb
@@ -55,6 +55,7 @@ module GetText
       @filename = nil
       @last_modified = nil
       @little_endian = true
+      @charset = nil
       @output_charset = output_charset
       @plural_proc = nil
       super()
@@ -157,7 +158,8 @@ module GetText
             msgstr = convert_encoding(msgstr, msgid)
           end
         end
-        self[convert_encoding(msgid, msgid)] = msgstr.freeze
+        msgid.force_encoding(@charset) if @charset
+        self[msgid] = msgstr.freeze
       end
       self
     end
