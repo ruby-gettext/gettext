ae226b19f839612b1d388d1acd1c19d9a23108cf
Sutou Kouhei <kou@clear-code.com>
2020-01-08 18:04:38 +0900

    Bump version

--- lib/gettext/version.rb
+++ lib/gettext/version.rb
@@ -1,7 +1,7 @@
 =begin
   version - version information of gettext
 
-  Copyright (C) 2012-2018 Kouhei Sutou <kou@clear-code.com>
+  Copyright (C) 2012-2020 Sutou Kouhei <kou@clear-code.com>
   Copyright (C) 2005-2009 Masao Mutoh
 
   You may redistribute it and/or modify it under the same
@@ -9,5 +9,5 @@
 =end
 
 module GetText
-  VERSION = "3.2.9"
+  VERSION = "3.3.0"
 end
