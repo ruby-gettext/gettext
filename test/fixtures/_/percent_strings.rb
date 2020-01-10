1cc891486fd592be2d4c64cfae931ff1d296ff4b
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2020-01-10 21:58:34 +0900

    Follow test module convention

--- test/fixtures/_/percent_strings.rb
+++ test/fixtures/_/percent_strings.rb
@@ -1,4 +1,29 @@
-%i(hello world)
-%s(hello world)
-%w(hello world)
-%x(echo hello world)
+module Fixtures
+  module Method_
+    class PercentStrings
+      include GetText
+
+      bindtextdomain("_", :path => Helper::Path.locale_path)
+
+      def symbol_array
+        _(%i(hello world))
+      end
+
+      def symbol
+        _(%s(hello world))
+      end
+
+      def string
+        _(%(hello world))
+      end
+
+      def string_array
+        _(%w(hello world))
+      end
+
+      def execute
+        _(%x(echo hello world))
+      end
+    end
+  end
+end
