c2fb2ab1ef7a087d146613bec3e7793fbffdbec6
Sutou Kouhei <kou@cozmixng.org>
2023-10-22 05:38:07 +0900

    Fix indent

--- test/test_po_entry.rb
+++ test/test_po_entry.rb
@@ -344,19 +344,19 @@ EOP
       assert_equal(expected_po, entry.to_s)
     end
 
-        def test_multiple_flags
-          entry = GetText::POEntry.new(:normal)
-          entry.msgid = "msgid"
-          entry.msgstr = "msgstr"
-          entry.flags = ["It's the flag.", "fuzzy"]
+    def test_multiple_flags
+      entry = GetText::POEntry.new(:normal)
+      entry.msgid = "msgid"
+      entry.msgstr = "msgstr"
+      entry.flags = ["It's the flag.", "fuzzy"]
 
-          expected_po = <<-EOP
+      expected_po = <<-EOP
 #, It's the flag., fuzzy
 msgid "msgid"
 msgstr "msgstr"
 EOP
-          assert_equal(expected_po, entry.to_s)
-        end
+      assert_equal(expected_po, entry.to_s)
+    end
 
     def test_previous
       entry = GetText::POEntry.new(:normal)
